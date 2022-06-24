ce20f264ed640dc3d8cfb5ec07e6b71ca79daee6
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-06-23 21:12:12 -0700

    most things set up
    need to figure out how to move

--- Perp.py
+++ Perp.py
@@ -7,29 +7,35 @@ from soupsieve import select
 class Person():
     def __init__(self) -> None:
         self = turtle.Turtle()
+        self.speed(0)
         self.shape("square")
         self.color("red")
-        self.goto(250, 250)
+        self.penup()
+        self.goto(0, 0)
 
-    def moveup(self):
-        y = self.ycor()
-        y += 20
-        self.sety(y)
 
-    def movedown(self):
-        y = self.ycor()
-        y -= 20
-        self.sety(y)
+def moveup(self):
+    y = self.ycor()
+    y += 20
+    self.sety(y)
 
-    def moveleft(self):
-        x = self.xcor()
-        x -= 20
-        self.setx(x)
 
-    def moveright(self):
-        x = self.xcor()
-        x += 20
-        self.setx(x)
+def movedown(self):
+    y = self.ycor()
+    y -= 20
+    self.sety(y)
+
+
+def moveleft(self):
+    x = self.xcor()
+    x -= 20
+    self.setx(x)
+
+
+def moveright(self):
+    x = self.xcor()
+    x += 20
+    self.setx(x)
 
 
 # close window with a button press
@@ -37,6 +43,11 @@ def close():
     sc.bye()
     sc.mainloop()
 
+def newscreen():
+    sc = turtle.Screen()
+    sc.title("One character")
+    sc.bgcolor("white")
+    sc.setup(width=500, height=500)
 
 perp = Person()
 sc = turtle.Screen()
@@ -46,10 +57,17 @@ sc.setup(width=500, height=500)
 
 
 # Keyboard bindings
-# TODO: hmm may need to make these global scope?
 sc.listen()
 sc.onkeypress(moveup, "w")
 sc.onkeypress(movedown, "s")
-sc.onkeypress(paddlebup, "Up")
-sc.onkeypress(paddlebdown, "Down")
+sc.onkeypress(moveleft, "a")
+sc.onkeypress(moveright, "d")
+sc.onkeypress(moveup, "Up")
+sc.onkeypress(movedown, "Down")
+sc.onkeypress(moveleft, "Left")
+sc.onkeypress(moveright, "Right")
 sc.onkeypress(close, "q")
+
+while True:
+    sc.update()
+    
