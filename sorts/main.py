a65fd3f3c1986447cef851c8e2a2b83734b9a7b5
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-07-01 19:11:10 -0700

    user input included

--- sorts/main.py
+++ sorts/main.py
@@ -5,6 +5,13 @@ import quicksort
 
 if __name__ == "__main__":
     arr = [12, 11, 13, 5, 6, 7]
-    mergesort.mergemain(arr)
-    bubblesort.bubblemain(arr)
-    quicksort.quickmain(arr)
+    while True:
+        x = input("\n1. Merge Sort\n2. Bubble Sort\n3. Quick Sort\nexit\n")
+        if x == "1":
+            mergesort.mergemain(arr)
+        elif x == "2":
+            bubblesort.bubblemain(arr)
+        elif x == "3":
+            quicksort.quickmain(arr)
+        elif x == "exit":
+            break
